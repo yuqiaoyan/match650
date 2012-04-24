b5ac46d39e02c0ec70472003bbd16bbac68823dc
Bonnie Yu <yuqiaoyan@gmail.com>
2012-04-24 10:32:48 -0400

    adjusted code in retrieval.py to use processed_aff

--- retrieval.py
+++ retrieval.py
@@ -4,8 +4,9 @@ import os
 import string
 import lucene
 from lucene import *
+from update_index import *
 #INDEX_DIR = "Tmp/REMOVEME.index-dir"
-INDEX_DIR = "Tmp/affiliation.index-dir"
+INDEX_DIR = "Tmp/preprocess_aff.index-dir"
 STUDENT_PATH = "getData/student_data/"
 MAX = 10
 output = ""
@@ -55,37 +56,38 @@ def search_by_student(student, analyzer, searcher,fieldList):
     for hit in hits.scoreDocs:
         write_to_file(str(hit.score))
         doc = searcher.doc(hit.doc)
+        write_to_file(doc.get("name"))
         write_to_file(doc.get("interest"))
         write_to_file('...................','...................')
         explain_score(searcher, hit.doc,query)
 
 if __name__ == "__main__":
+#cmd line:	python retrieval [resultFile] [fields]
+#			python retrieval v3Explain.txt interest processed_aff
     searcher, analyzer = initial_searcher()
     file_list = os.listdir(STUDENT_PATH)
     fieldList = sys.argv[2:]
     
     resultName = sys.argv[1]
     resultFile = open(sys.argv[1],"w")
-    
+    hLine = '___________________________'
 
     for f in os.listdir(STUDENT_PATH):
         print 'opening', STUDENT_PATH + f
         reader = csv.reader(open(STUDENT_PATH + f, 'r'), delimiter=',', quotechar='"')
         for line in reader:
-            #map(write_to_file,line)
             student_profile = {}
             student_profile['name'] = line[0]
             student_profile['interest'] = line[1]
-            student_profile['affiliation'] = line[2]
-            write_to_file('searching'+student_profile['name'])
-            write_to_file('interests are'+student_profile['interest'])
-            write_to_file('affiliation is'+student_profile['affiliation'])
+            student_profile['affiliationRaw'] = line[2]
+            student_profile['processed_aff'] = " ".join(rebuild_affiliation(line[2]))
+            write_to_file('SEARCHING '+student_profile['name'])
+            write_to_file('INTERESTS ARE '+student_profile['interest'])
+            write_to_file('AFFILIATION IS '+student_profile['affiliationRaw'])
             write_to_file('.......')
-            write_to_file('result is:')
+            write_to_file('RESULT IS:')
             search_by_student(student_profile, analyzer, searcher,fieldList)
-            write_to_file('___________________________')
-            write_to_file('___________________________')
-            write_to_file('___________________________')
+            write_to_file(hLine,hLine,hLine)
 
     resultFile.write(output.encode("utf-8"))
     
