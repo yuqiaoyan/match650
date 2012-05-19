1bb5f0a0391bcbe8ee40deb71f45fe3480c0cf72
Bonnie Yu <yuqiaoyan@gmail.com>
2012-05-19 12:19:09 -0400

    blah

--- insertDB.py
+++ insertDB.py
@@ -1,16 +1,29 @@
 import MySQLdb as mdb
 import sys
 
-try:
-	con = mdb.connect('localhost','root','kddmatcher','kdd');
-	cur = con.cursor()
-	cur.execute("SELECT VERSION()")
-	data = cur.fetchone()
-	print "Database version: %s " % data
+#CONFIGURATIONS#
+host	= 'localhost'
+user	= 'root'
+pwd	= 'kddmatcher'
+db	= 'kdd'
 
-except mdb.Error, e:
-	print "Error %d: %s" % (e.args[0],e.args[1])
-	sys.exit(1)
-finally:
-	if con:
-		con.close()
+def connectDB():
+#returns a cursor object to our database
+	try:
+		con = mdb.connect(host,user,pwd,db)
+		cur = con.cursor()
+		return cur
+
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	#finally:
+	#	if con:
+	#		con.close()
+
+def insertProf(profDict,cur):
+	cur.execute("
+		INSERT INTO professor
+		SET	phone = %s
+			email = %s",/
+		(profDict["phone"],profDict["email"))	
