6c14aa7cd27e4eff2b1dc6844a36920a3ca1bc5d
Bonnie Yu <yubonnie@ip-10-212-98-210.ec2.internal>
2012-05-20 17:34:59 +0000

    moved build_indexing to getdata folder

--- getData/insertDB.py
+++ getData/insertDB.py
@@ -1,6 +1,6 @@
 import MySQLdb as mdb
 import sys
-import json
+from build_indexing import *
 
 #CONFIGURATIONS#
 host	= 'localhost'
@@ -40,12 +40,13 @@ def insertProf(profDict,cur,con):
 			bio = %s,\
 			pictureURL = %s,\
 			coauthorID = %s,\
-			interest = %s" % 
-		(profDict["Phone"],profDict["Email"],profDict["Eomepage"],\
+			interest = %s,\
+			arnetID = %s",  
+		(profDict["Phone"],profDict["Email"],profDict["Homepage"],\
 		profDict["Position"],profDict["Affiliation"],profDict["Address"],\
-		profDict["Phduniv"],profDict["Ahdmajor"],profDict["Asuniv"],\
-		profDict["Bio"],profDict["AictureURL"],profDict["AoauthorID"],\
-		profDict["Interest"]))
+		profDict["Phduniv"],profDict["Phdmajor"],profDict["Bsuniv"],\
+		profDict["Bio"],profDict["PictureURL"],profDict["CoauthorID"],\
+		profDict["Interest"],profDict["Id"]))
 	con.commit()	
 
 def rawDictToProfDict(rawDict):
@@ -56,13 +57,37 @@ def rawDictToProfDict(rawDict):
 			rawDict[key] = None
 	return rawDict
 
-#ToDo: grab interest data
-#ToDo: check that interest data exists
-#ToDo: if interest data, does not exist, then do not add to DB
-# else 
-#ToDo: grab coauthor data
-#add it to DB
 
 def parseJSONLine(text):
 	data = json.loads(text)
 	return(data[0])
+
+def getInsertData():
+#main function to get professor data
+#and insert into the database
+
+	con, cur = connectDB()
+
+	file = open("committees")
+	i = 0
+	for line in file:
+		if(i > 2):
+			break
+		i+=1
+		rawDict = parseJSONLine(line)		
+		#ToDo: grab interest data
+		rawDict["Interest"] = get_interest_by_id(str(rawDict["Id"]))
+		print "interest is",rawDict["Interest"]
+#ToDo: check that interest data exists
+#ToDo: if interest data, does not exist, then do not add to DB
+# else 
+#ToDo: grab coauthor data
+		#convert dict to a well formed dictionary
+		#all keys in profKeys must exist in dict
+		profDict = rawDictToProfDict(rawDict)
+
+		#add professor to DB
+		#insertProf(profDict,cur,con)
+
+	file.close()
+		
