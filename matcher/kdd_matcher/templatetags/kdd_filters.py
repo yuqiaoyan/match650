00cdb5984013b828994ec7b05e6f2279b5f8d256
Ke Wu <ke.wu@ibeca.me>
2012-06-18 03:38:18 +0000

    fix star raty, add bold interest

--- matcher/kdd_matcher/templatetags/kdd_filters.py
+++ matcher/kdd_matcher/templatetags/kdd_filters.py
@@ -1,9 +1,30 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter(name='split')
 @stringfilter
 def split(value, delimiter):
+    print 'in split'
     return value.split(delimiter)
+
+
+@register.filter(needs_autoescape=True)
+@stringfilter
+def int_bold(value, interest, autoescape=None):
+    new_string = []
+    print 'in filter'
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    for item in value.split(' '):
+        if item.lower() in interest.lower():
+            new_string.append("<strong>%s</strong>" % esc(item))
+        else:
+            new_string.append("%s" % esc(item))
+    result = ' '.join(new_string)
+    return mark_safe(result)
